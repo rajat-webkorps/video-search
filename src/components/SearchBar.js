1c4a9ad4a49fdd490491923024bb846f16b4ed18
rajat-webkorps <rajat.a@webkorps.com>
2024-04-11 18:46:39 +0530

    dropdown-check

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,19 @@ const SearchBar = ({ handleFormSubmit }) => {
 
     const handleSubmit = event => {
         event.preventDefault();
-        handleFormSubmit(term);
+        if (selectedItem === 'Select Link Type') {
+            alert("Please select a link type from the dropdown.");
+        }
+        else if (
+            (selectedItem === 'Youtube Video' && term.includes('watch')) ||
+            (selectedItem === 'Youtube Channel' && term.includes('com/@')) ||
+            (selectedItem === 'KeyWords' && !term.includes('www.youtube.com'))
+        ) {
+            handleFormSubmit(term);
+        }
+        else {
+            alert("Please enter detail same as selected dropdown.");
+        }
     };
 
     return (
@@ -27,12 +39,13 @@ const SearchBar = ({ handleFormSubmit }) => {
                                 {selectedItem}
                             </button>
                             <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <p className="dropdown-item" onClick={() => handleItemClick('KeyWords')}>KeyWords</p>
                                 <p className="dropdown-item" onClick={() => handleItemClick('Youtube Video')}>Youtube Video</p>
-                                <p className="dropdown-item"onClick={() => handleItemClick('Youtube Channel')}>Youtube Channel</p>
+                                <p className="dropdown-item" onClick={() => handleItemClick('Youtube Channel')}>Youtube Channel</p>
                             </div>
                         </div>
                         <input className="form-control form-control-lg col-sm-10" type="text" onChange={handleChange} name='video-search' placeholder="Enter Link" />
-                        
+
                     </div>
                 </form>
             </div>
