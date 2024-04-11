1b27a76471c3f315f42b99510abe9fb6bb4cdc9c
rajat-webkorps <rajat.a@webkorps.com>
2024-04-11 14:00:31 +0530

    videoList-onlyvideoshow

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,13 +2,22 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, handleVideoSelect }) => {
-    const renderedVideos = videos.map((video) => {
-
+    const renderedVideos = videos.filter(video => video !== null && video.id.videoId!==undefined).map((video) => {
+        
         if (video.id.kind === "youtube#channel") {
             return null;
         }
         return <VideoItem key={`${video.id.videoId}-${video.snippet.title}`} video={video} handleVideoSelect={handleVideoSelect} />;
     });
+    
+    // =====================Video List==========================
+        const videoListObject ={ids:[]};
+        renderedVideos.forEach((video) => {
+            if (video !== null && video.props.video.id.videoId!==undefined) {
+                videoListObject.ids.push(video.props.video.id.videoId);
+            }
+        })
+    // =====================Video List End==========================
 
     return (
         <section className="gallery-block cards-gallery">
